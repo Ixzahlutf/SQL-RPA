7afcac3066229166cf642139222f3dcad1f8a532
Izzah Luthfiah <izzahluthfiah@gmail.com>
2025-07-30 18:12:18 +0700

    Menambahkan folder TugasMinggu2 untuk laporan paralel testing

---
+++ Tugas_Minggu_2/src/test/java/com/juaracoding/ecom/test1.java
@@ -0,0 +1,10 @@
+package com.juaracoding.ecom;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+    @Test
+    public void delay5Seconds() throws InterruptedException{
+        Thread.sleep(5000);
+    }
+}
---
+++ Tugas_Minggu_2/src/test/java/com/juaracoding/ecom/test2.java
@@ -0,0 +1,10 @@
+package com.juaracoding.ecom;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+    @Test
+    public void delay7Seconds() throws InterruptedException{
+        Thread.sleep(7000);
+    }
+}
